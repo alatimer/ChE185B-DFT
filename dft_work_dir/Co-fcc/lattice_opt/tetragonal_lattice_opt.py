13b8616006fde0bd109bdff78a84cfa224c2c1c9
Shankara Krishna Anand <sanand94@sherlock-ln03.stanford.edu>
2018-04-16 11:15:39 -0700

    Cobalt 111 lattice opt

--- dft_work_dir/Co-fcc/lattice_opt/tetragonal_lattice_opt.py
+++ dft_work_dir/Co-fcc/lattice_opt/tetragonal_lattice_opt.py
@@ -1,19 +1,19 @@
 #!/home/vossj/suncat/bin/python
 
-#SBATCH -p iric
+#SBATCH -p normal
 #SBATCH --output=myjob.out
 #SBATCH --error=myjob.err
 #SBATCH --time=48:00:00                                 #default is 20 hours
 #SBATCH --nodes=1
 #SBATCH --mem-per-cpu=4000
 #SBATCH --mail-type=END,FAIL                            #get emailed about job BEGIN, END, or FAIL
-#SBATCH  --mail-user=allegralatimer@gmail.com
+#SBATCH  --mail-user=sanand94@stanford.edu
 #SBATCH --ntasks-per-node=16                            #task to run per node; each node has 16 cores
 
 
 import sys
-path='/home/alatimer/che185b-drm/src/ase'
-sys.path.insert(0,path)
+#path='/home/sanand94/che185b-drm/src/ase'
+#sys.path.insert(0,path)
 from ase import build
 #import structures
 from scipy.optimize import *
@@ -26,13 +26,13 @@ import math
 
 import sys
 
-Mtl='Ni'
+Mtl='Co'
 a0=2.5 ##starting guesses for lattice constant
 
 x0=[a0]  ##pack up vaariables to optimize over to pass to solver (in this case there is only one)
 iter = 0
 logfile = open('lattice_opt.log','a')
-logfile.write('Energy\ta\tb\tc\n')
+logfile.write('Energy\ta\n')
 logfile.close()
 atoms_list = []
 
@@ -44,7 +44,7 @@ def get_energy(x):
     calc = espresso(pw=600, #while optimizing lattice, use high pw/dw and kpts. We will optimize these later.
         dw = 6000,
         kpts = (12,12,12),
-        nbands = -100,
+        nbands = -10,
         xc = 'RPBE',
         convergence = {'energy':1e-5,
                        'mixing':0.1,
@@ -60,15 +60,16 @@ def get_energy(x):
 
     atoms = build.bulk(Mtl,crystalstructure='fcc',a=x[0])  #make a fcc unit cell with new lattice vector
     for atom in atoms:
-        atom.magmom=2
+        atom.magmom=4
     atoms_list.append(atoms)
 
     atoms.set_calculator(calc)
     write('out.traj',atoms_list)
+    a=x[0] 
 
     energy = atoms.get_potential_energy()
     logfile = open('lattice_opt.log','a')
-    logfile.write('%s\t%s\t%s\t%s\n' %(energy,a,b,c))
+    logfile.write('%s\t%s\n' %(energy,a))
     logfile.close()
     
     return energy
